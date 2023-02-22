c88d562e2b30b5750232f796d30ae90727e3e305
codermuyi <kasopeadeola@gmail.com>
2023-02-22 01:55:05 +0100

    Included demo data

---
+++ assets/demo_data.js
@@ -0,0 +1,171 @@
+const data = [
+  {
+  adult: false,
+  backdrop_path: "/cG5QZHyIRJXqo53YA41gbNMlpIM.jpg",
+  id: 717980,
+  title: "Sharper",
+  original_language: "en",
+  original_title: "Sharper",
+  overview: "A small, wealthy family in New York City gets progressively torn apart by secrets, lies, and the theft that orchestrates all of it.",
+  poster_path: "/tq8x5F17q95T1j0up5rpzXFAylN.jpg",
+  media_type: "movie",
+  genre_ids: [
+  53,
+  80,
+  18
+  ],
+  popularity: 111.538,
+  release_date: "2023-02-10",
+  video: false,
+  vote_average: 7.3,
+  vote_count: 64
+  },
+  {
+  adult: false,
+  backdrop_path: "/8YFL5QQVPy3AgrEQxNYVSgiPEbe.jpg",
+  id: 640146,
+  title: "Ant-Man and the Wasp: Quantumania",
+  original_language: "en",
+  original_title: "Ant-Man and the Wasp: Quantumania",
+  overview: "Super-Hero partners Scott Lang and Hope van Dyne, along with with Hope's parents Janet van Dyne and Hank Pym, and Scott's daughter Cassie Lang, find themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will push them beyond the limits of what they thought possible.",
+  poster_path: "/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg",
+  media_type: "movie",
+  genre_ids: [
+  12,
+  878,
+  35
+  ],
+  popularity: 2075.264,
+  release_date: "2023-02-10",
+  video: false,
+  vote_average: 6.511,
+  vote_count: 514
+  },
+  {
+  adult: false,
+  backdrop_path: "/pux5yTjmL1yGjBuhzdBStuQb6jc.jpg",
+  id: 785084,
+  title: "The Whale",
+  original_language: "en",
+  original_title: "The Whale",
+  overview: "A reclusive English teacher suffering from severe obesity attempts to reconnect with his estranged teenage daughter for one last chance at redemption.",
+  poster_path: "/ken2sQbCqZuAYmgPknrChXvkrEK.jpg",
+  media_type: "movie",
+  genre_ids: [
+  18
+  ],
+  popularity: 725.577,
+  release_date: "2022-12-09",
+  video: false,
+  vote_average: 7.6,
+  vote_count: 145
+  },
+  {
+  adult: false,
+  backdrop_path: "/gH0FHLbOgDIxCkwwzgh14pdnc5p.jpg",
+  id: 631842,
+  title: "Knock at the Cabin",
+  original_language: "en",
+  original_title: "Knock at the Cabin",
+  overview: "While vacationing at a remote cabin, a young girl and her two fathers are taken hostage by four armed strangers who demand that the family make an unthinkable choice to avert the apocalypse. With limited access to the outside world, the family must decide what they believe before all is lost.",
+  poster_path: "/dm06L9pxDOL9jNSK4Cb6y139rrG.jpg",
+  media_type: "movie",
+  genre_ids: [
+  27,
+  9648,
+  53
+  ],
+  popularity: 222.77,
+  release_date: "2023-02-01",
+  video: false,
+  vote_average: 6.589,
+  vote_count: 331
+  },
+  {
+  adult: false,
+  backdrop_path: "/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg",
+  id: 505642,
+  title: "Black Panther: Wakanda Forever",
+  original_language: "en",
+  original_title: "Black Panther: Wakanda Forever",
+  overview: "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
+  poster_path: "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+  media_type: "movie",
+  genre_ids: [
+  28,
+  12,
+  878
+  ],
+  popularity: 4378.531,
+  release_date: "2022-11-09",
+  video: false,
+  vote_average: 7.451,
+  vote_count: 3353
+  },
+  {
+  adult: false,
+  backdrop_path: "/b1Y8SUb12gPHCSSSNlbX4nB3IKy.jpg",
+  id: 315162,
+  title: "Puss in Boots: The Last Wish",
+  original_language: "en",
+  original_title: "Puss in Boots: The Last Wish",
+  overview: "Puss in Boots discovers that his passion for adventure has taken its toll: He has burned through eight of his nine lives, leaving him with only one life left. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.",
+  poster_path: "/kuf6dutpsT0vSVehic3EZIqkOBt.jpg",
+  media_type: "movie",
+  genre_ids: [
+  16,
+  12,
+  35,
+  10751,
+  14
+  ],
+  popularity: 3204.321,
+  release_date: "2022-12-07",
+  video: false,
+  vote_average: 8.485,
+  vote_count: 3887
+  },
+  {
+  adult: false,
+  backdrop_path: "/hogFjtmJlEIARfFQvdBsKANYG9a.jpg",
+  id: 740441,
+  title: "Unlocked",
+  original_language: "ko",
+  original_title: "스마트폰을 떨어뜨렸을 뿐인데",
+  overview: "A woman’s life is turned upside-down when a dangerous man gets a hold of her lost cell phone and uses it to track her every move.",
+  poster_path: "/oGHr8yUOXtVK0mGoZURZqqbwXXj.jpg",
+  media_type: "movie",
+  genre_ids: [
+  53,
+  9648
+  ],
+  popularity: 114.453,
+  release_date: "2023-02-17",
+  video: false,
+  vote_average: 7.354,
+  vote_count: 41
+  },
+  {
+  adult: false,
+  backdrop_path: "/1xhcEecvRJXQ2OAVO7l9btlrN6D.jpg",
+  id: 667216,
+  title: "Infinity Pool",
+  original_language: "en",
+  original_title: "Infinity Pool",
+  overview: "While staying at an isolated island resort, James and Em are enjoying a perfect vacation of pristine beaches, exceptional staff, and soaking up the sun. But guided by the seductive and mysterious Gabi, they venture outside the resort grounds and find themselves in a culture filled with violence, hedonism, and untold horror.",
+  poster_path: "/5DNRr2juXdwtvktwXxwuk9Usk8O.jpg",
+  media_type: "movie",
+  genre_ids: [
+  27,
+  878,
+  53
+  ],
+  popularity: 129.167,
+  release_date: "2023-01-27",
+  video: false,
+  vote_average: 6.6,
+  vote_count: 93
+  },
+]
+
+export default data
