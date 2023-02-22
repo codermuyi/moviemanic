e3a8ace491dc83e231606ac23295e9071b69da64
codermuyi <kasopeadeola@gmail.com>
2023-02-22 03:23:42 +0100

    Added media query breakpoints

---
+++ assets/breakpoints.js
@@ -0,0 +1,11 @@
+const breakpoints = {
+  xs: '(max-width: 600px)',
+  sm: '(min-width: 600px)',
+  md: '(min-width: 768px)',
+  lg:'(min-width: 992px)',
+  xl:'(min-width: 1200px)',
+  xxl:'(min-width: 1440px)',
+  xxxl:'(min-width: 2560px)',
+}
+
+export default breakpoints
