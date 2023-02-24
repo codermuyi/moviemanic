c445180998e4f9f6e6ad820acf725d6760c9af9e
codermuyi <kasopeadeola@gmail.com>
2023-02-24 19:42:21 +0100

    Added card hover effects

--- src/MovieCard.tsx
+++ src/MovieCard.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import Image from 'next/image'
 import breakpoints from '@/assets/breakpoints'
 
@@ -22,6 +22,7 @@ const MovieCard = ({
 
   return (
     <Card>
+      <span></span>
       <Image
         // src='/last-of-us.jpeg'
         src={`https://image.tmdb.org/t/p/w1280${imgSrc}`}
@@ -45,9 +46,59 @@ const MovieCard = ({
   )
 }
 
+const animate = keyframes`
+  0%, 100% {
+    transform: translateY(10px);
+  }
+  
+  50% {
+    transform: translate(-10px);
+  }
+`
+
 const Card = styled.div`
   min-height: 150px;
   position: relative;
+  transition: 0.5s;
+  cursor: pointer;
+  transform-style: preserve-3d;
+  perspective: 200px;
+
+  :hover {
+    .card-image.normal {
+      /* transform: translateY(-20px) rotateX(7deg); */
+      transform: translateY(-8%) rotateX(7deg);
+      box-shadow: 0 5px 15px rgba(0,0,0,0.28);
+    }
+    .card-image.trending {
+      transform: scale(.95);
+    }
+    .trending-info {
+      transform: translate(10px, -10px);
+    }
+
+    span:after {
+      width: 50px;
+      height: 50px;
+      opacity: 1;
+    }
+  }
+
+  span::after {
+    content:'';
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    background: rgba(255, 255, 255, 0.1);
+    opacity: 0;
+    transition: 0.5s;
+    animation: ${animate} 2s ease-in-out infinite;
+    box-shadow: 0 5px 15px rgba(0,0,0,0.08);
+    animation-delay: -1s;
+  }
 
   .card-image {
     object-fit: cover;
@@ -55,6 +106,7 @@ const Card = styled.div`
     border-radius: 20px;
     width: auto;
     height: auto;
+    transition: 0.3s;
   }
   
   .normal {
@@ -95,6 +147,7 @@ const CardInfo = styled.div`
     position: absolute;
     left: 1rem;
     bottom: 1rem;
+    transition: 0.3s;
   }
 
   .normal-info {
