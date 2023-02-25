bd260807254167794257403bd1f577c61a7080fe
codermuyi <kasopeadeola@gmail.com>
2023-02-25 18:07:43 +0100

    Added link to portfolio

--- src/global/Footer.tsx
+++ src/global/Footer.tsx
@@ -11,6 +11,8 @@ const Footer = () => {
         width={200}
         height={50}
       />
+      <br />
+      <p>Created by <a href='https://samuel-adepoju.vercel.app'>Samuel Adepoju</a></p>
     </Foot>
   )
 }
@@ -20,6 +22,10 @@ const Foot = styled.footer`
   place-items: center;
   padding-block: 2rem;
   flex-direction: column;
+
+  a {
+    color: rgb(var(--theme-main-color));
+  }
 `
 
 export default Footer
