b701d13e6f624a59c77ec62cf85cf74a00d1739a
codermuyi <kasopeadeola@gmail.com>
2023-02-27 00:28:55 +0100

    Movie page refactor

--- pages/movies/[id]/index.tsx
+++ pages/movies/[id]/index.tsx
@@ -1,61 +1,32 @@
-import Meta from '@/src/Meta'
-import styled from 'styled-components'
-import PageLayout from '@/src/global/PageLayout'
-import breakpoints from '@/assets/breakpoints'
-import FilmPoster from '@/src/FilmPoster'
-import FilmInfo from '@/src/FilmInfo'
-import SimilarFilms from '@/src/SimilarFilms'
+import FilmPageContent from "@/src/FilmPageContent"
 
-const moviePage = ({ data, credits, similar, videoData }: { [key: string]: any }) => {
-  const trailerID = videoData.results.filter((videoData: any, i: number) => videoData.type === 'Trailer')[0]?.key
-
+const moviePage = ({
+  data,
+  credits,
+  similar,
+  videoData
+}: { [key: string]: any }) => {
   return (
-    <>
-      <Meta
-        title={`${data.title} | Moviemanic`}
-        description={data.overview}
-      />
-
-      <PageLayout>
-        <PageBody>
-          <FilmPoster
-            path={data.poster_path}
-          />
-          <FilmInfo
-            {...data}
-            credits={credits}
-            trailerID={trailerID}
-          />
-          <SimilarFilms data={similar.results} />
-        </PageBody>
-      </PageLayout>
-    </>
+    <FilmPageContent
+      data={data}
+      credits={credits}
+      similar={similar}
+      videoData={videoData}
+    />
   )
 }
 
-const PageBody = styled.div`
-  @media ${breakpoints.md} {
-    display: grid;
-    grid-template-columns: 350px minmax(10px, 1fr);
-    grid-template-rows: 2;
-    max-width: 2000px;
-  }
-`
-
 export const getServerSideProps = async (ctx: any) => {
-  const key = process.env.NEXT_PUBLIC_TMDB_API_KEY
   const api_path = `https://api.themoviedb.org/3/movie/${ctx.params.id}`
+  const key = `?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
 
-  const res = await fetch(`${api_path}?api_key=${key}`)
+  const res = await fetch(`${api_path}${key}`)
   const data = await res.json()
-
-  const res2 = await fetch(`${api_path}/credits?api_key=${key}`)
+  const res2 = await fetch(`${api_path}/credits${key}`)
   const credits = await res2.json()
-
-  const res3 = await fetch(`${api_path}/recommendations?api_key=${key}`)
+  const res3 = await fetch(`${api_path}/recommendations${key}`)
   const similar = await res3.json()
-
-  const res4 = await fetch(`${api_path}/videos?api_key=${key}`)
+  const res4 = await fetch(`${api_path}/videos${key}`)
   const videoData = await res4.json()
 
   return {
