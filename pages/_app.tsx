54d88cc98e4f18489db84defd22ead25e6039215
codermuyi <kasopeadeola@gmail.com>
2023-03-08 00:15:11 +0100

    fix: Solve clashing images problem in pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'simplebar-react/dist/simplebar.min.css';
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  return <Component key={router.asPath} {...pageProps} />
 }
