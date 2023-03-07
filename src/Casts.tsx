844c1023239b6acc251cfa4318c23096d72298b9
codermuyi <kasopeadeola@gmail.com>
2023-03-07 01:27:50 +0100

    Casts component refactor

--- src/Casts.tsx
+++ src/Casts.tsx
@@ -1,36 +1,16 @@
 import Image from 'next/image'
 import styled from 'styled-components'
-import { ProfileIcon } from './global/SVGIcons'
 import SimpleBar from 'simplebar-react';
-import { useState } from 'react'
+import CastProfile from './CastProfile'
 
-const Casts = ({ credits }: { credits: any }) => {
-  const [src, setSrc] = useState('')
-  
+const Casts = ({ credits }: { credits: any }) => {  
   return (
     <FilmCast>
       <h2 className='heading'>Casts</h2>
       <SimpleBar>
         <div className='cast-list'>
           {credits.cast.map((cast: any, i: number) => (
-            <div key={i} className='cast'>
-              {cast.profile_path ?
-                <Image
-                  src={src === '' ? `https://image.tmdb.org/t/p/w1280${cast.profile_path}`: src}
-                  alt={cast.name}
-                  width={150}
-                  height={150}
-                  style={{ objectFit: 'cover' }}
-                  className='cast-profile'
-                  onError={() => setSrc('/error-image.png')}
-                /> :
-                <ProfileIcon fill='gray' />
-              }
-              <div className='cast-info'>
-                <p>{cast.name}</p>
-                <p className='role'>{cast.character && 'Played ' + cast.character}</p>
-              </div>
-            </div>
+            <CastProfile key={i} cast={cast} />
           ))}
         </div>
       </SimpleBar>
