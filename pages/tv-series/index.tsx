856b5abe2e9893dd7cfb82f09ee58a3ed3fe2443
codermuyi <kasopeadeola@gmail.com>
2023-03-08 21:46:49 +0100

    feat: implemented demo pages

---
+++ pages/tv-series/index.tsx
@@ -0,0 +1,16 @@
+import PageLayout from "@/src/global/PageLayout"
+import Meta from '@/src/Meta'
+
+const index = () => {
+  return (
+    <>
+    <Meta />
+
+    <PageLayout>
+      <h1>TV Series : (</h1>
+    </PageLayout>
+    </>
+  )
+}
+
+export default index
