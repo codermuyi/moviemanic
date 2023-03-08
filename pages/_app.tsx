8b0e448bf5c2d0c23c70ef6e232dfa8ba2f94eab
codermuyi <kasopeadeola@gmail.com>
2023-03-08 23:27:54 +0100

    feat: add page loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,20 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'simplebar-react/dist/simplebar.min.css';
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import NProgress from 'nprogress';
+import '@/styles/nprogress.css'
+
+// NProgress.configure({ showSpinner: false }); 
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () =>  NProgress.start());
+    router.events.on('routeChangeComplete', () =>  NProgress.done());
+    router.events.on('routeChangeError', () =>  NProgress.done());
+  }, [router]);
+
   return <Component key={router.asPath} {...pageProps} />
 }
