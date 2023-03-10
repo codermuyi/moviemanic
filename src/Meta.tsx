9a251f6b4856bb846eba3a9e55ec0a30939b0663
codermuyi <kasopeadeola@gmail.com>
2023-03-10 03:35:38 +0100

    meta tags repositioning

--- src/Meta.tsx
+++ src/Meta.tsx
@@ -7,12 +7,13 @@ const Meta = ({ title, description }: { [key: string]: string }) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="theme-color" content="rgb(78, 100, 160)" />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="description" content={description} />
+      
+      <meta name="theme-color" content="rgb(78, 100, 160)" />
 
       <meta name="og:type" content="website" />
       <meta name="og:title" content={title} />
-
       <meta
         name="og:url"
         content={`https://moviemanic.vercel.app${router.asPath}`}
@@ -21,7 +22,6 @@ const Meta = ({ title, description }: { [key: string]: string }) => {
       <meta name="og:image" content='/moviemanic-512x512.png' />
 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
     </Head>
 
   )
@@ -29,7 +29,7 @@ const Meta = ({ title, description }: { [key: string]: string }) => {
 
 Meta.defaultProps = {
   title: 'Moviemanic',
-  description: 'Search for any movie and tv show and watch trailers'
+  description: 'Search for movies and tv shows, and watch trailers directly on the site'
 }
 
 export default Meta
