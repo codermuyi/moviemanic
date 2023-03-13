98d7e9ca23bd4ba66b0e82ddb29b44afed95084e
codermuyi <kasopeadeola@gmail.com>
2023-03-14 00:50:57 +0100

    NavLink improvements

--- src/atoms/NavLink.tsx
+++ src/atoms/NavLink.tsx
@@ -3,26 +3,34 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import styled from 'styled-components'
 
-const NavLink = ({ href, children }: any) => {
+const NavLink = ({ href, children, className }: any) => {
   const [ariaCurrent, setAriaCurrent] = useState<'page' | undefined>()
   const { asPath } = useRouter()
 
+  console.log(asPath)
+
   useEffect(() => {
-    const ariaCurrent = href === asPath ? "page" : undefined
+    const ariaCurrent = href === decodeURIComponent(asPath) ? "page" : undefined
 
     setAriaCurrent(ariaCurrent)
   }, [asPath, href]);
 
   return (
     <Link href={href} legacyBehavior>
-      <MyLink aria-current={ariaCurrent}>{children}</MyLink>
+      <MyLink href={href} className={className} aria-current={ariaCurrent}>{children}</MyLink>
     </Link>
   );
 };
 
 const MyLink = styled.a`
+  cursor: pointer;
+
   &[aria-current] {
     color: rgb(186,79,58);
+
+    :hover {
+      color: rgb(186,79,58) !important;
+    }
   }
 `
 
