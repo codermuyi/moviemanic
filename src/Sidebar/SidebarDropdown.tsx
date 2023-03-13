cc47d4a1037e8aed7afecf47cd4503acb701d761
codermuyi <kasopeadeola@gmail.com>
2023-03-13 01:08:43 +0100

    Added border-color hover effect

--- src/Sidebar/SidebarDropdown.tsx
+++ src/Sidebar/SidebarDropdown.tsx
@@ -81,6 +81,11 @@ const Dropdown = styled.div.attrs(props => {
     background-image: url("data:image/svg+xml,%3csvg width='3%' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23333' stroke-width='2' stroke-dasharray='6%2c 21%2c 7' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
     border-right: 0;
     margin-left: 5px;
+    transition: border-color .3s;
+
+    :hover {
+      border-left-color: rgba(var(--theme-main-color));
+    }
   }
 
   .dropdown-item {
