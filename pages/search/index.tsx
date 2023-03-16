5a0a7d3e88a0a48786539af943a1ca37f3f52ba2
codermuyi <kasopeadeola@gmail.com>
2023-03-16 05:35:46 +0100

    Demo search page

---
+++ pages/search/index.tsx
@@ -0,0 +1,13 @@
+import PageLayout from "@/src/Layout/PageLayout"
+import Meta from '@/src/atoms/Meta'
+
+const search = () => {
+  return (
+    <>
+      <Meta title='Search | Moviemanic' />
+      <PageLayout />
+    </>
+  )
+}
+
+export default search
