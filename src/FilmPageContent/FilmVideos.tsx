cbb9cc0d8fd1ab6fc15161669632ce5ddfa4c992
codermuyi <kasopeadeola@gmail.com>
2023-03-16 08:33:37 +0100

    Add icon to videos button

--- src/FilmPageContent/FilmVideos.tsx
+++ src/FilmPageContent/FilmVideos.tsx
@@ -3,6 +3,8 @@ import Trailer from './FilmYoutubeVideo'
 import Button from '../atoms/Button'
 import { useState } from 'react';
 import MoreVideosModal from './FilmVideosModal';
+import rightIcon from '@/assets/icons/arrow-right-2.svg'
+import Image from 'next/image'
 
 const FilmVideos = ({ videoData }: any) => {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -20,7 +22,10 @@ const FilmVideos = ({ videoData }: any) => {
             padding='.7rem'
             onClick={() => setIsOpen(true)}
           >
-            Videos <span>&lt;_&gt;</span>
+            <span>Videos</span>
+            <span className='icon'>
+              <Image src={rightIcon} alt='right' width={20} height={20} />
+            </span>
           </Button>
         }
       </div>
@@ -37,24 +42,31 @@ const Content = styled.div`
   margin-block: 2rem;
 
   .button {
-    margin-block: 1rem;
+    margin-block: 1.1rem;
     margin-inline: auto;
-    display: block;
     width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
     span {
-      display: inline-block;
-      width: 0;
-      overflow: hidden;
       transition-duration: inherit;
+
+      &.icon {
+
+        display: inline-block;
+        /* width: 0; */
+        overflow: hidden;
+        transform: scale(0) translateX(0);
+      }
     }
 
     :hover {
       background-color: white;
       color: rgb(var(--theme-main-color));
 
-      span {
-        width: 25px;
+      span.icon {
+        transform: scale(1) translateX(5px);
       }
     }
   }
