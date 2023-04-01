f196f8a5a4c2bb398e4d281001f1b77cfdb2ade0
codermuyi <kasopeadeola@gmail.com>
2023-04-01 15:23:07 +0100

    Update to supabase insert

--- src/FilmPageContent/FilmPoster.tsx
+++ src/FilmPageContent/FilmPoster.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 import Button from 'src/atoms/Button'
+import Toast from '../Toast'
 import breakpoints from '@/assets/breakpoints'
 // import useSupabase from '../useSupabase'
 import { myFetch } from '@/assets/utilities'
@@ -14,27 +15,12 @@ const FilmPoster = ({ path, info, mediaType }: { path: string, info: any, mediaT
   const supabase = useSupabaseClient()
   const session = useSession()
   const { data: profile } = useSwr('/api/profile-details', myFetch)
-
+  const [toastMessage, setToastMessage] = useState('')
+  const [toastOpen, setToastOpen] = useState(false)
   // const [supabase, session] = useSupabase()
-  console.log(mediaType, info)
 
   async function setFilmInfo() {
-    // const { status } = await supabase
-    //   .from('film_list')
-    //   .insert([
-    //     {
-    //       media_type: mediaType,
-    //       film_id: info?.id,
-    //       user_id: session?.user.id,
-    //       title: info?.title || info?.name,
-    //       year: parseInt(info?.release_date || info?.first_air_date),
-    //       poster_path: path,
-    //       backdrop_path: info?.backdrop_path,
-    //       genres: info?.genres.map((g: any) => g.name),
-    //       status: info?.status
-    //     }
-    //   ])
-    if (info !== null) {
+    if (info) {
       const { status } = await supabase
         .from('film_list')
         .insert([
@@ -47,9 +33,20 @@ const FilmPoster = ({ path, info, mediaType }: { path: string, info: any, mediaT
             poster_path: path,
             backdrop_path: info.backdrop_path,
             genres: info.genres.map((g: any) => g.name),
-            status: info.status
+            status: info.status,
+            // To avoid duplicates for each user
+            dup: session?.user.id + info.id
           }
         ])
+
+      if (status === 201) {
+        setToastOpen(true)
+        setToastMessage('Added film to your list')
+      }
+      if (status === 409) {
+        setToastOpen(true)
+        setToastMessage('Film already in your list')
+      }
 
       console.log('Status: ', status)
     }
@@ -57,6 +54,11 @@ const FilmPoster = ({ path, info, mediaType }: { path: string, info: any, mediaT
 
   return (
     <Poster className='film-poster'>
+      <Toast 
+        open={toastOpen}
+        setOpen={setToastOpen}
+        message={toastMessage}
+      />
       <div className='sticky'>
         <Image
           src={src}
