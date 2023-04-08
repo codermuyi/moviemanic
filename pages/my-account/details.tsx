736873e42e943e9192ca13dd3501b2180d96a887
codermuyi <kasopeadeola@gmail.com>
2023-04-08 02:31:05 +0100

    Added content to details poage

--- pages/my-account/details.tsx
+++ pages/my-account/details.tsx
@@ -3,14 +3,23 @@ import { GetServerSidePropsContext } from 'next';
 import Meta from '@atoms/Meta';
 import CheckForUsername from '@atoms/CheckForUsername';
 import { routeGuard } from '@/src/routeGuard';
+import { formatDate } from '@/src/helpers';
 
-const DetailsPage = ({ profile }: any) => {
+const DetailsPage = ({ profile, user }: any) => {
   return (
     <>
       <Meta title='Details | Moviemanic' />
       <CheckForUsername profile={profile}>
-        <div>
-          <h1>Details</h1>
+        <div style={{ paddingInline: '1rem' }}>
+          <h1 style={{ paddingBlock: '1rem' }}>Details</h1>
+          <p>Username: {profile?.username}</p>
+          <br />
+          <p>Email: {user.email}</p>
+          <br />
+          <p>User ID: {user.id}</p>
+          <br />
+          <p>Date Created: {formatDate(profile?.created_at)}</p>
+          <br />
         </div>
       </CheckForUsername>
     </>
